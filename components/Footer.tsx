86fed4a18e70567aaaf907830e4c21c1d3a27c2b
Taha Shahid <tahashahid203@gmail.com>
2024-07-11 07:18:41 +0400

    uploading so that I can test this on vercel and see if the site is atleast fast when deployed. if not, i'll have to make a new portfolio

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
 import { socialMedia } from "@/data";
+import Image from "next/image";
 
 const Footer = () => {
   return (
@@ -34,7 +35,7 @@ const Footer = () => {
         <div className="flex items-center md:gap-3 gap-6">
             {socialMedia.map((profile)=>(
                 <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
-                    <img src={profile.img} alt={profile.id}width={20} height={20}/>
+                    <Image src={profile.img} alt={JSON.stringify(profile.id)} width={20} height={20}/>
                 </div>
             ))}
         </div>
